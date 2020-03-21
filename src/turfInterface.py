719367e1bfa491bc8e74ba5f6e1754e82d9b454f
jobvanellen <jobvanellen@kpnmail.nl>
2020-03-21 16:11:32 +0100

    yessins update met huisknop en 140 melding

--- src/turfInterface.py
+++ src/turfInterface.py
@@ -4,20 +4,24 @@ from time import sleep
 
 app = App(title="Bazaar Turflijst")
 app.tk.attributes("-fullscreen",True)
-turfknopArea = Box(app, layout="grid", align="left", width = "fill", height = "fill")
+turfknopArea = Box(app, layout="grid", align="left", width="fill", height="fill")
 #settingknopArea = Box(app,align="left", width="fill", height="fill")
 textArea = Box(app, align="left", width="fill", height="fill")
 standText = TextBox(textArea, enabled=False,text="stand",
                     align="left",multiline=True, width = "fill", height = "fill")
-standText.text_size = 24
+standText.text_size = 23
 doubleCheckWindow = Window(app,title="Zeker weten?", height=300, width=500, visible=False)
 doubleCheckText = Text(doubleCheckWindow, text="Weet je zeker dat je de stand wilt resetten?", align="top")
-doublecheckText2 = Text(doubleCheckWindow, text="Dit kan niet ongedaan gemaakt woren."
-                       , align="top")
-voWindow = Window(app,title="73 bravo",
-                  height=300, width=500, visible=False)
+doublecheckText2 = Text(doubleCheckWindow, text="Dit kan niet ongedaan gemaakt worden.", align="top")
+
+voWindow = Window(app, title="73 bravo", height=300, width=500, visible=False)
 voText = Text(voWindow, text="GEFELICITEERD JIJ ONTZETTENDE VOBAAS", align="top")
-voText2 = Text(voWindow, text="73 pilsies gemurdered", align="top")
+voText2 = Text(voWindow, text="73 pilsies gemurderd", align="top")
+
+superVoWindow = Window(app, title="Bierlijscht gewonnen maat", height=300, width=500, visible=False)
+superVoText = Text(superVoWindow, text="LEKKER GEZOPEN OUWE!", align="top")
+superVoText2 = Text(superVoWindow, text="Je hebt de bierlijst gewonnen", align="top")
+superVoText3 = Text(superVoWindow, text="Check ff op wie je anytimers hebt", align="top")
 
 huisgenoten = []
 laatsteTurfjes = []
@@ -29,8 +33,15 @@ def turf(persoon):
    print(persoon['naam'])
    persoon['turfjes'] += 1
    writeDict()
-   if persoon ['turfjes'] == 73:
+
+   if persoon['turfjes'] == 73:
       openVo()
+   elif persoon['turfjes'] == 140:
+      for persooncheck in huisgenoten:
+         if persooncheck['turfjes'] >= 140 and persooncheck != persoon:
+            break
+      openSuperVo()
+   
    updateHistory(persoon)
    updateStand()
 
@@ -55,8 +66,15 @@ def openVo():
    closeVo()
    voWindow.show()
 
+def openSuperVo():
+   closeSuperVo()
+   superVoWindow.show()
+
 def closeVo():
    voWindow.hide()
+
+def closeSuperVo():
+   superVoWindow.hide()
 
 def openCheck():
    closeCheck()
@@ -77,9 +95,9 @@ def updateStand():
    stand = ""
    for persoon in huisgenoten:
       stand = stand + persoon['naam'] + ": " + str(persoon['turfjes']) + "\n"
-   standText.enabled=True
-   standText.value=stand
-   standText.enabled=False
+   standText.enabled = True
+   standText.value = stand
+   standText.enabled = False
 
 def updateHistory(persoon):
    if len(laatsteTurfjes) >= 20:
@@ -96,6 +114,11 @@ def undo():
    persoon['turfjes'] -= 1
    writeDict()
    updateStand()
+
+# def huisTurf():
+#    readDict()
+#    turf(huisgenoten.get("naam", "Huis"))
+# )
    
 #widgets
 huisgenoten = readDict()
@@ -106,22 +129,27 @@ for persoon in huisgenoten:
     x = int(iterator % 4)
     y = int(iterator / 4)
     turfKnoppen.append(PushButton(turfknopArea, command=turf, args=[persoon],
-                                  text = persoon['naam'],image=persoon['foto'],
+                                  text=persoon['naam'], image=persoon['foto'],
                                   padx=0, pady=0, grid=[x,y,1,1])
                        )
     iterator = iterator + 1
+    if iterator > 15: 
+       break
 
 updateStand()
 
-resetKnop = PushButton(turfknopArea, command=openCheck, text="Reset",grid=[1,4], width="13", height="2")
-undoKnop = PushButton(turfknopArea, command=undo,text="Undo", grid=[2,4], width="13", height="2")
+resetKnop = PushButton(turfknopArea, command=openCheck, text="Reset", grid=[0,4], width="13", height="2")
+undoKnop = PushButton(turfknopArea, command=undo, text="Undo", grid=[1,4], width="13", height="2")
+huisTurfKnop = PushButton(turfknopArea, command=turf, args=[huisgenoten[16]], text="Huis", grid=[3,4], width="13", height="2")
 resetKnop.text_size = 15
 undoKnop.text_size = 15
+huisTurfKnop.text_size = 15
 
 confirmKnop = PushButton(doubleCheckWindow, command=reset, text="Ja, reset stand", align="left")
 denyKnop = PushButton(doubleCheckWindow, command=closeCheck, text="Nee, annuleren", align="right")
 
 voKnop = PushButton(voWindow, command=closeVo, text="VO!", align="top")
+superVoKnop = PushButton(superVoWindow, command=closeSuperVo, text="VO!", align="top")
 #turfMelding = Text(app, color="blue", size=36, text="Turf!", visible="true", width="fill", height="fill")
 
 updateStand()
